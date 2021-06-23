5458e3a31dff4e29d9b75b66215f2c09d11a0ee9
Hussein-bc <hussein.akram@bellevuecollege.edu>
2021-06-23 12:57:40 -0700

    first commit

---
+++ Final/src/test/java/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+}
+
+
+@Test
+public void Number () {
+
+}
